fde69056784c64306150543f3147876d5bfe2b9e
LordeP26 <ped.li2608@gmail.com>
2025-01-30 19:42:57 -0300

    Category

---
+++ course/src/main/java/com/pedrolima/course/resources/CategoryResource.java
@@ -0,0 +1,33 @@
+package com.pedrolima.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedrolima.course.entities.Category;
+import com.pedrolima.course.services.CategoryService;
+
+@RestController //controlador rest do recurso web
+@RequestMapping(value = "/categories") //nome do recurso
+public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
