5b5483a1bbb44500e4c5b159417726ee7eabb99b
LordeP26 <ped.li2608@gmail.com>
2025-02-05 14:34:26 -0300

    User inser

--- course/src/main/java/com/pedrolima/course/resources/UserResource.java
+++ course/src/main/java/com/pedrolima/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.pedrolima.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pedrolima.course.entities.User;
 import com.pedrolima.course.services.UserService;
@@ -29,5 +33,13 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping //inserir um novo recurso no banco
+	public ResponseEntity<User> insert( @RequestBody User obj){ //o requestbody é a conversão do json desserializado
+		obj = service.insert(obj);
+		// estrturura pra gerar o 201 na resposta
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); //conversão do tipo de dado pro created aceitar
+		return  ResponseEntity.created(uri).body(obj);
 	}
 }
